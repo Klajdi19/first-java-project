29a83b5175a217319c9a29a78aed3dab80617f3f
Klajdi19 <klajdithaci1@gmail.com>
2021-10-13 17:28:39 +0200

    first java program

---
+++ src/JavaFun.java
@@ -0,0 +1,7 @@
+public class JavaFun {
+    public static void main(String[] args) {
+        System.out.println("Klajdi");
+        System.out.println(23);
+        System.out.println("Tiranë");
+    }
+}
